bec013a102bd9fea55748a609f8a68ee3c35354c
Vlad0si4 <boiko.vlad91@gmail.com>
2023-05-27 19:30:23 +0300

    add_hw3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_KEY = 'videoplayer-current-time'
+
+player.on('timeupdate', throttle(function (event) {
+    localStorage.setItem(CURRENT_KEY, JSON.stringify(event.seconds));
+  }, 1000)
+);
+const currentTime = localStorage.getItem(CURRENT_KEY);
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+const debounce = require('lodash.debounce');
+import localStorageAPI from './localstorage.js';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+
+const FEEDBACK_KEY = "feedback-form-state"
+const userInfo = {};
+
+const fillContactFormFields = () => {
+  const userInfoFromLS = localStorageAPI.load(FEEDBACK_KEY)
+
+  if (userInfoFromLS === undefined) {
+    return;
+  }
+
+    for (const key in userInfoFromLS) {
+    feedbackForm.elements[key].value = userInfoFromLS[key]
+  };
+}
+
+fillContactFormFields();
+
+const onContactFormFieldInput = event => {
+  const feedbackFormEl = event.target;
+  const contactValue = feedbackFormEl.value;
+  const contactName = feedbackFormEl.name;
+
+  userInfo[contactName] = contactValue
+
+  localStorageAPI.save(FEEDBACK_KEY, userInfo)
+  
+}
+
+const onContactFormSubmit = event => {
+  event.preventDefault();
+  const contactFormReset = event.target;
+
+  if (contactFormReset.elements.email.value === '') {
+    alert('Please enter your email.');
+  } 
+    localStorageAPI.remove(FEEDBACK_KEY);
+    contactFormReset.reset();
+    console.log(userInfo);
+  
+};
+feedbackForm.addEventListener('input', debounce(onContactFormFieldInput, 500));
+feedbackForm.addEventListener('submit', onContactFormSubmit)
